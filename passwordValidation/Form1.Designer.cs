5759223f4c148becf046763562f3d4d5f218f039
JoshRambert <Joshua_rambert@yahoo.com>
2017-12-06 02:53:42 -0800

    adding to the repo"

---
+++ passwordValidation/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace passwordValidation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.passwrdButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A valid Password must meet these three requirments ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "1 - It must be atleast 8 characters long";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "2 - It must contain at least one uppercase letter";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(232, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "3 - It must contain at leasat one lowercase letter";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(57, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(210, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "4 - it must contain at least one numeric digit";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(100, 127);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Enter a Password";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(90, 152);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(111, 20);
+            this.passwordTextBox.TabIndex = 6;
+            // 
+            // passwrdButton
+            // 
+            this.passwrdButton.Location = new System.Drawing.Point(48, 187);
+            this.passwrdButton.Name = "passwrdButton";
+            this.passwrdButton.Size = new System.Drawing.Size(75, 42);
+            this.passwrdButton.TabIndex = 7;
+            this.passwrdButton.Text = "Check password";
+            this.passwrdButton.UseVisualStyleBackColor = true;
+            this.passwrdButton.Click += new System.EventHandler(this.passwrdButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(169, 187);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 42);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 261);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.passwrdButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Password Validation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button passwrdButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ passwordValidation/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace passwordValidation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //the Number uppercase method accepts a string argument 
+        // and returns the number of uppercase letters it contains.
+        private int NumberUpperCase(string str)
+        {
+            int upperCase = 0;  //the number of uppercase letters
+            
+            //count tthe uppercase characters in str.
+            foreach (char ch in str)
+            {
+                if (char.IsUpper(ch))
+                {
+                    upperCase++;
+                }
+            }
+
+            //return the number of uppercasse characters 
+            return upperCase;
+        }
+
+        //the numberLowerCase method accepts a string argument 
+        // and returns the number of lowercase letters it contains 
+        private int NumberLowerCase(string str)
+        {
+            int lowerCase = 0;  // the number of lowercase letters 
+
+            //count the lowercase characters in str
+            foreach(char ch in str)
+            {
+                if (char.IsLower(ch))
+                {
+                    lowerCase++;
+                }
+            }
+
+            //return the number of lower case characters 
+            return lowerCase;
+        } 
+
+        // The numberDigits method  accepts a string arguent 
+        // and returns the number of numeric digits it contains 
+        private int NumberDigits(string str)
+        {
+            int digits = 0;     // the storage to hold the digits
+
+            //count the digits in the string 
+            foreach(char ch in str)
+            {
+                if(char.IsDigit(ch))
+                {
+                    digits++;
+                }
+            }
+            //return the number of digits 
+            return digits;
+        }
+
+        private void passwrdButton_Click(object sender, EventArgs e)
+        {
+            const int MIN_LENGTH = 8;   //passwords minimum length 
+
+            //get the password from the textbox 
+            string password = passwordTextBox.Text;
+
+            //validate the password
+            if (password.Length >= MIN_LENGTH &&
+                NumberUpperCase(password) >= 1 &&
+                NumberLowerCase(password) >= 1 &&
+                NumberDigits(password) >=1)
+            {
+                MessageBox.Show("The password is valid");
+            }
+            else
+            {
+                MessageBox.Show("The password is not valid.. and does not meet the requirements");
+            }
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close the form 
+            this.Close();
+        }
+    }
+}
